b3bcfd481395caf114a241dec7690a7dd7d3fd4b
theeacx <licxandruteodora@gmail.com>
2023-10-26 00:25:35 -0700

    Nimic imp, vr doar sa vad daca mai stiu sa fac

--- scripts/second-page.js
+++ scripts/second-page.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const addButton= document.getElementById('add_btn');
         addButton.addEventListener('click', function() {
-            window.location.ref= 'third-page.html';
+            window.location.href= 'third-page.html';
         });
 
         const deleteButton= document.getElementById('delete_btn');
