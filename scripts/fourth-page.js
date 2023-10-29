edcfec22d5e7048b354740568659a9d3583b454d
theeacx <licxandruteodora@gmail.com>
2023-10-28 21:29:51 -0700

    imaginea vietii

--- scripts/fourth-page.js
+++ scripts/fourth-page.js
@@ -1,20 +1,56 @@
 window.onload=function(){
-    let canvas=document.getElementById('photo');
+    let canvas=document.getElementById('destination-photo');
     let context =canvas.getContext('2d');
 
    
     let canvasWidth=canvas.width;
     let canvasHeight=canvas.height;
 
-    const fruitImage=new Image();
-    fruitImage.src="/media/fructe.png";
+    let img=new Image();
+    // img.src="/media/fructe.png";
 
-    //context.drawImage(url,x,y,width,height)
-    fruitImage.onload=function(){
-    context.drawImage(fruitImage,0,0,canvas.width,canvas.height);
+    // //context.drawImage(url,x,y,width,height)
+    // img.onload=function(){
+    // context.drawImage(img,0,0,canvas.width,canvas.height);
+    // }
+
+    // img.onerror = function () {
+    //     console.error("Failed to load the image.");
+    // };
+    // JavaScript to update the content and title based on the clicked link
+    const urlParams = new URLSearchParams(window.location.search);
+    const destinationTitle = urlParams.get("title");
+    const destinationDescription = urlParams.get("description");
+    const destinationImage = urlParams.get("photo");
+    console.log(destinationImage);
+    console.log("-------------------" + destinationTitle);
+
+    const titleElement = document.getElementById("destination-title");
+    const headingElement = document.getElementById("destination-heading");
+    const descriptionElement = document.getElementById(
+      "destination-description"
+    );
+    if(destinationImage){
+        img.src=destinationImage;
+        console.log(img.src);
+
+        //context.drawImage(url,x,y,width,height)
+        img.onload=function(){
+            context.clearRect(0, 0, canvasWidth, canvasHeight);
+            context.drawImage(img,0,0,canvas.width,canvas.height);
+        }
+
+        img.onerror = function () {
+            console.error("Failed to load the image.");
+        };
+    }
+    if (destinationTitle) {
+      titleElement.textContent = destinationTitle;
+      headingElement.textContent = `About ${destinationTitle}`;
     }
 
-    fruitImage.onerror = function () {
-        console.error("Failed to load the image.");
-    };
+    if (destinationDescription) {
+      descriptionElement.textContent = destinationDescription;
+    }
+    
 }
