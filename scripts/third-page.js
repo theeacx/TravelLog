f88038d54e777e95c428f771b3d0711b9d02cbd9
roxanaP04 <petrescugabriela21@stud.ase.ro>
2023-11-01 10:49:12 +0200

    backButton

--- scripts/third-page.js
+++ scripts/third-page.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    //go back button
+    //add button
     const backButton= document.getElementById("add_btn");
     backButton.addEventListener("click", function(){
+        window.location.href = "second-page.html";
+    });
+
+    //go back button
+    const backButton2= document.getElementById("goBack");
+    backButton2.addEventListener("click", function(){
         window.location.href = "second-page.html";
     });
 
