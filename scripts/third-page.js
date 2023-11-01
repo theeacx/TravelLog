33d27390f458c7bb7666e736c2adc94b7ef12399
theeacx <licxandruteodora@gmail.com>
2023-10-31 23:07:43 -0700

    btn modificat putin

--- scripts/third-page.js
+++ scripts/third-page.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //go back button
-    const backButton= document.getElementById("goBack");
+    const backButton= document.getElementById("add_btn");
     backButton.addEventListener("click", function(){
         window.location.href = "second-page.html";
     });
